f033f95747d58d3faf346f5d1ee0e2ab6e8b4810
Andrey <andydeshko@gmail.com>
2023-12-01 23:54:41 +0200

    added brief simulation description

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
-
+// Customers arriving at random intervals using exponential distribution and then the service point retrieves them
+// from the queue and serves them at random time using normal distribution.
+// then we are able to observe how long the client has stayed in our system (time in queue + service time).
 public class Main {
 
     public static void main(String[] args) {
