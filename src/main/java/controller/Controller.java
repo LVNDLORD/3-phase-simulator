e4f53109591087d883f797d467edd20ea4b7e842
Arman Yerkesh <a.yerkesh@gmail.com>
2023-12-04 10:19:36 +0200

    Add loggers and rename MyEngine to Simulation

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,6 +1,18 @@
 package controller;
 
+import model.Simulation;
+
 public class Controller {
     // When user interacts with interface - update simulation state
     // When simulation's state changes - update interface
+
+    private Simulation sim = new Simulation(this);
+
+    public Controller() {
+        log("Controller initialized");
+    }
+
+    private void log (String s) {
+        System.out.println(s);
+    }
 }
--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -7,6 +7,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class View extends Application {
+
+    public View() {
+        log("View initialized");
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/interface.fxml"));
@@ -14,5 +19,9 @@ public class View extends Application {
 
         stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    private void log (String s) {
+        System.out.println(s);
     }
 }
