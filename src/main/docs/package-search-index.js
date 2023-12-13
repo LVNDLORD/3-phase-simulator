f1ee5d704459fbd1ef3ee71ec5a11b8ececb0247
Arman Yerkesh <a.yerkesh@gmail.com>
2023-12-13 11:25:03 +0200

    Generate docs

---
+++ src/main/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"db"},{"l":"model"},{"l":"model.eduni.distributions"},{"l":"view"}];updateSearchResults();
